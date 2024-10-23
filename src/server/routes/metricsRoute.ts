1c05bb9f67698c50ee5077d95f32187d2023dec6
Jonathan Paul <jonathan.paul@qualityisaculture.com>
2024-10-23 19:33:44 +0200

    Basic Jira Integration

--- src/server/routes/metricsRoute.ts
+++ src/server/routes/metricsRoute.ts
@@ -3,11 +3,30 @@ const metricsRoute = express.Router();
 import {
   TypedResponse as TR,
   TypedRequestQuery as TRQ,
-  TypedRequestBody as TRB
+  TypedRequestBody as TRB,
 } from '../../Types';
 
-metricsRoute.get('/metrics', (req: TRQ<{}>, res: TR<{message: string}>) => {
-  res.json({ message: 'Metrics route' });
+metricsRoute.get('/metrics', (req: TRQ<{}>, res: TR<{ message: string, data: string }>) => {
+  const domain = process.env.JIRA_DOMAIN;
+  const issueKey = 'AF-1';
+  const url = `${domain}/${issueKey}`;
+  const email = process.env.JIRA_EMAIL;
+  const apiToken = process.env.JIRA_API_TOKEN;
+  let responseData;
+  fetch(url, {
+    method: 'GET', // or 'POST', 'PUT', etc. depending on your request
+    headers: {
+      Authorization: 'Basic ' + btoa(`${email}:${apiToken}`),
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      responseData = data;
+      res.json({ message: 'Metrics route', data: responseData });
+    })
+    .catch((error) => console.error('Error:', error));
+
 });
 
 export { metricsRoute };
