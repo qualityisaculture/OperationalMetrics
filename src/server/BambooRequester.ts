354da4342cc8087ef8b20ae4891ef296cbc3184b
qualityisaculture <jonathan.paul@qualityisaculture.com>
2024-12-20 11:10:26 +0000

    Bamboo will now request data on first call of each day

--- src/server/BambooRequester.ts
+++ src/server/BambooRequester.ts
@@ -6,80 +6,96 @@ type BambooAPIBuild = {
 type BambooAPIBuilds = {
   results: {
     result: BambooAPIBuild[];
-  }
-}
+  };
+};
 type BambooAPIDetailBuild = {
   buildNumber: number;
   buildCompletedDate: string;
   state: string;
   restartCount?: number;
-}
+};
 type BambooBuildData = {
   buildNumber: number;
   buildState: string;
   buildDate: string;
   restartCount: number;
   allData: BambooAPIDetailBuild;
-}
+};
 export default class BambooRequester {
-    cache: { [key: string]: BambooBuildData[] };
-    constructor() {
-      this.cache = {}
-    }
+  cache: { [key: string]: BambooBuildData[] };
+  cacheDate: string;
+  constructor() {
+    this.cache = {};
+    this.cacheDate = "";
+  }
 
-    async getBuildsFromStartIndex(projectBuildKey: string, startIndex: number) {
-      const domain = process.env.BAMBOO_DOMAIN;
-      const url = `${domain}/rest/api/latest/result/${projectBuildKey}.json?start-index=${startIndex}`;
-      let results: BambooAPIBuilds = await this.fetchRequest(url);
-      let buildNumbers = results.results.result.map((build) => build.buildNumber);
-      let allBuilds = await Promise.all(buildNumbers.map((buildNumber) => this.getBuild(projectBuildKey, buildNumber)));
-      return allBuilds;
+  async getBuildsFromStartIndex(projectBuildKey: string, startIndex: number) {
+    const domain = process.env.BAMBOO_DOMAIN;
+    const url = `${domain}/rest/api/latest/result/${projectBuildKey}.json?start-index=${startIndex}`;
+    let results: BambooAPIBuilds = await this.fetchRequest(url);
+    let buildNumbers = results.results.result.map((build) => build.buildNumber);
+    let allBuilds = await Promise.all(
+      buildNumbers.map((buildNumber) =>
+        this.getBuild(projectBuildKey, buildNumber)
+      )
+    );
+    return allBuilds;
+  }
+
+  async getBuild(
+    buildResultKey: string,
+    buildNumber: number
+  ): Promise<BambooBuildData> {
+    const domain = process.env.BAMBOO_DOMAIN;
+    const url = `${domain}/rest/api/latest/result/${buildResultKey}-${buildNumber}.json`;
+    let buildData: BambooAPIDetailBuild = await this.fetchRequest(url);
+    let returnData: BambooBuildData = {
+      buildNumber: buildData.buildNumber,
+      buildState: buildData.state,
+      buildDate: buildData.buildCompletedDate,
+      restartCount: buildData.restartCount || 0,
+      allData: buildData,
     };
+    return returnData;
+  }
 
-    async getBuild(buildResultKey: string, buildNumber: number): Promise<BambooBuildData> {
-      const domain = process.env.BAMBOO_DOMAIN;
-      const url = `${domain}/rest/api/latest/result/${buildResultKey}-${buildNumber}.json`;
-      let buildData: BambooAPIDetailBuild = await this.fetchRequest(url);
-      let returnData: BambooBuildData = {
-        buildNumber: buildData.buildNumber,
-        buildState: buildData.state,
-        buildDate: buildData.buildCompletedDate,
-        restartCount: buildData.restartCount || 0,
-        allData: buildData,
-      };
-      return returnData;
+  async getLastXBuilds(
+    projectBuildKey: string,
+    numberOfBuilds: number = 100
+  ): Promise<BambooBuildData[]> {
+    let cacheDate = new Date().toISOString().split("T")[0];
+    if (this.cache[projectBuildKey] && this.cacheDate === cacheDate) {
+      return this.cache[projectBuildKey];
     }
+    let builds: BambooBuildData[] = [];
+    let startIndex = 0;
+    while (startIndex < numberOfBuilds) {
+      let response = await this.getBuildsFromStartIndex(
+        projectBuildKey,
+        startIndex
+      );
+      builds = builds.concat(response);
+      startIndex += 25;
+    }
+    this.cache[projectBuildKey] = builds;
+    this.cacheDate = cacheDate;
+    return builds;
+  }
 
-    async getLastXBuilds(projectBuildKey: string, numberOfBuilds: number = 100): Promise<BambooBuildData[]> {
-      if (this.cache[projectBuildKey]) {
-        return this.cache[projectBuildKey];
-      }
-      let builds: BambooBuildData[] = [];
-      let startIndex = 0;
-      while (startIndex < numberOfBuilds) {
-        let response = await this.getBuildsFromStartIndex(projectBuildKey, startIndex);
-        builds = builds.concat(response);
-        startIndex += 25;
-      }
-      this.cache[projectBuildKey] = builds;
-      return builds;
+  async fetchRequest(url: string) {
+    const apiToken = process.env.BAMBOO_API_TOKEN;
+    const response = await fetch(url, {
+      method: "GET", // or 'POST', 'PUT', etc. depending on your request
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+        Accept: "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(
+        "Failed to fetch  data: " + url + " " + response.statusText
+      );
     }
-
-    
-    async fetchRequest(url: string) { 
-      const apiToken = process.env.BAMBOO_API_TOKEN;
-      const response = await fetch(url, {
-        method: 'GET', // or 'POST', 'PUT', etc. depending on your request
-        headers: {
-          Authorization: `Bearer ${apiToken}`,
-          Accept: 'application/json',
-        },
-      });
-      if (!response.ok) {
-        throw new Error(
-          'Failed to fetch  data: ' + url + ' ' + response.statusText
-        );
-      }
-      return response.json();
-    }
+    return response.json();
+  }
 }
--- test/server/BambooRequester.test.ts
+++ test/server/BambooRequester.test.ts
@@ -4,44 +4,44 @@ let fetchResponseOk = (body) => ({
   json: () => Promise.resolve(body),
 });
 
-describe('BambooRequester', () => {
+describe("BambooRequester", () => {
   let fetchMock;
 
   beforeEach(() => {
-    fetchMock = jest.spyOn(global, 'fetch');
+    fetchMock = jest.spyOn(global, "fetch");
     jest.resetModules();
-    process.env.BAMBOO_DOMAIN = 'localhost:8080';
+    process.env.BAMBOO_DOMAIN = "localhost:8080";
   });
 
-  describe('getBuildsFromStartIndex', () => {
-    it('request builds from server', async () => {
+  describe("getBuildsFromStartIndex", () => {
+    it("request builds from server", async () => {
       let bambooRequester = new BambooRequester();
       fetchMock.mockResolvedValueOnce(
         fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
+          results: {
+            result: [{ buildNumber: 1 }, { buildNumber: 2 }],
+          },
+        })
+      );
       fetchMock.mockResolvedValueOnce(
         fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
+          buildSummary: {
+            buildCompletedDate: "2021-01-01T00:00:00.000Z",
+            state: "Successful",
+          },
+        })
+      );
       fetchMock.mockResolvedValueOnce(
         fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));
-      let response = await bambooRequester.getBuildsFromStartIndex('AA-BB', 0);
+          buildSummary: {
+            buildCompletedDate: "2021-01-02T00:00:00.000Z",
+            state: "Failed",
+          },
+        })
+      );
+      let response = await bambooRequester.getBuildsFromStartIndex("AA-BB", 0);
       expect(fetchMock.mock.calls[0][0]).toEqual(
-        'localhost:8080/rest/api/latest/result/AA-BB.json?start-index=0'
+        "localhost:8080/rest/api/latest/result/AA-BB.json?start-index=0"
       );
       // expect(response).toEqual({
       //   results: {
@@ -52,423 +52,134 @@ describe('BambooRequester', () => {
       //   },
       // });
     });
-    
   });
 
-  describe('getBuild', () => {
-    it('request build from server', async () => {
+  describe("getBuild", () => {
+    it("request build from server", async () => {
       let bambooRequester = new BambooRequester();
       fetchMock.mockResolvedValueOnce(
         fetchResponseOk({
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
+          buildCompletedDate: "2021-01-02T00:00:00.000Z",
+          state: "Failed",
           buildNumber: 1,
-      }));
-      let response = await bambooRequester.getBuild('AA-BB', 1);
+        })
+      );
+      let response = await bambooRequester.getBuild("AA-BB", 1);
       expect(fetchMock.mock.calls[0][0]).toEqual(
-        'localhost:8080/rest/api/latest/result/AA-BB-1.json'
+        "localhost:8080/rest/api/latest/result/AA-BB-1.json"
       );
       expect(response).toEqual({
-          buildDate: '2021-01-02T00:00:00.000Z',
-          buildState: 'Failed',
-          buildNumber: 1,
-          restartCount: 0,
-          allData: expect.any(Object),
+        buildDate: "2021-01-02T00:00:00.000Z",
+        buildState: "Failed",
+        buildNumber: 1,
+        restartCount: 0,
+        allData: expect.any(Object),
       });
     });
-
   });
 
-  describe('getLast1000Builds', () => {
-    it('request 1000 from server first time', async () => {
+  describe("getLastXBuilds", () => {
+    function mockResponse(state1 = "Successful", state2 = "Failed") {
+      fetchMock.mockResolvedValueOnce(
+        fetchResponseOk({
+          results: {
+            result: [{ buildNumber: 1 }, { buildNumber: 2 }],
+          },
+        })
+      );
+      fetchMock.mockResolvedValueOnce(
+        fetchResponseOk({
+          buildSummary: {
+            buildCompletedDate: "2021-01-01T00:00:00.000Z",
+            state: state1,
+          },
+        })
+      );
+      fetchMock.mockResolvedValueOnce(
+        fetchResponseOk({
+          buildSummary: {
+            buildCompletedDate: "2021-01-02T00:00:00.000Z",
+            state: state2,
+          },
+        })
+      );
+    }
+    it("request 100 from server first time", async () => {
       let bambooRequester = new BambooRequester();
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));
-      let response = await bambooRequester.getLastXBuilds('AA-BB');
+      mockResponse();
+      mockResponse();
+      mockResponse();
+      mockResponse();
+      let response = await bambooRequester.getLastXBuilds("AA-BB");
       expect(fetchMock).toHaveBeenCalledTimes(12);
       expect(fetchMock.mock.calls[0][0]).toEqual(
-        'localhost:8080/rest/api/latest/result/AA-BB.json?start-index=0'
+        "localhost:8080/rest/api/latest/result/AA-BB.json?start-index=0"
       );
-      expect(fetchMock.mock.calls[1][0]).toEqual(
-        'localhost:8080/rest/api/latest/result/AA-BB-1.json'
+      expect(fetchMock.mock.calls[3][0]).toEqual(
+        "localhost:8080/rest/api/latest/result/AA-BB.json?start-index=25"
+      );
+      expect(fetchMock.mock.calls[6][0]).toEqual(
+        "localhost:8080/rest/api/latest/result/AA-BB.json?start-index=50"
+      );
+      expect(fetchMock.mock.calls[9][0]).toEqual(
+        "localhost:8080/rest/api/latest/result/AA-BB.json?start-index=75"
       );
       expect(response.length).toEqual(8); //because only returning 2, but should return 25;
     });
 
-    it('caches the results', async () => {
-      let bambooRequester = new BambooRequester();
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));
-      let response = await bambooRequester.getLastXBuilds('AA-BB');
-      expect(fetchMock).toHaveBeenCalledTimes(12);
+    describe("caching", () => {
+      it("caches the results if requesting the same project on the same day", async () => {
+        let bambooRequester = new BambooRequester();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        let response = await bambooRequester.getLastXBuilds("AA-BB");
+        expect(fetchMock).toHaveBeenCalledTimes(12);
 
-      
-      let response2 = await bambooRequester.getLastXBuilds('AA-BB');
-      expect(fetchMock).toHaveBeenCalledTimes(12);
-      expect(response).toEqual(response2);
-    });
+        let response2 = await bambooRequester.getLastXBuilds("AA-BB");
+        expect(fetchMock).toHaveBeenCalledTimes(12);
+        expect(response).toEqual(response2);
+      });
 
-    it('caches the results per projectBuildKey', async () => {
-      let bambooRequester = new BambooRequester();
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Failed',
-        },
-      }));fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        results: {
-          result: [
-            { buildNumber: 1 },
-            { buildNumber: 2 },
-          ],
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-01T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      fetchMock.mockResolvedValueOnce(
-        fetchResponseOk({
-        buildSummary: {
-          buildCompletedDate: '2021-01-02T00:00:00.000Z',
-          state: 'Successful',
-        },
-      }));
-      let response = await bambooRequester.getLastXBuilds('AA-BB');
-      expect(fetchMock).toHaveBeenCalledTimes(12);
+      it("caches the results per projectBuildKey on the same day", async () => {
+        let bambooRequester = new BambooRequester();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse("Successful", "Successful");
+        let response = await bambooRequester.getLastXBuilds("AA-BB");
+        expect(fetchMock).toHaveBeenCalledTimes(12);
 
-      
-      let response2 = await bambooRequester.getLastXBuilds('AA-CC');
-      expect(fetchMock).toHaveBeenCalledTimes(24);
-      expect(response).not.toEqual(response2);
+        let response2 = await bambooRequester.getLastXBuilds("AA-CC");
+        expect(fetchMock).toHaveBeenCalledTimes(24);
+        expect(response).not.toEqual(response2);
+      });
+
+      it("won't cache the results if requesting the same project on different days", async () => {
+        jest.useFakeTimers();
+        jest.setSystemTime(new Date("2021-01-01"));
+        let bambooRequester = new BambooRequester();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse();
+        mockResponse("Successful", "Successful");
+        let response = await bambooRequester.getLastXBuilds("AA-BB");
+        expect(fetchMock).toHaveBeenCalledTimes(12);
+        jest.setSystemTime(new Date("2021-01-02"));
+
+        let response2 = await bambooRequester.getLastXBuilds("AA-BB");
+        expect(fetchMock).toHaveBeenCalledTimes(24);
+        expect(response).not.toEqual(response2);
+      });
     });
   });
 });
