091b6c35260943f360ab230081c7fa4e5d528a15
qualityisaculture <jonathan.paul@qualityisaculture.com>
2025-01-28 14:07:43 +0000

    Added Favicon

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,23 +1,29 @@
 //Create a default express server and listen on port 3000
-import express from 'express';
-import path from 'path';
-import { metricsRoute } from './routes/metricsRoute';
+import express from "express";
+import path from "path";
+import { metricsRoute } from "./routes/metricsRoute";
 
 let app;
 
-const Server = (mode: 'default' | 'dev' | 'e2e', application?: Express.Application) => {
+const Server = (
+  mode: "default" | "dev" | "e2e",
+  application?: Express.Application
+) => {
   app = application || express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, '.')));
+  app.use(express.static(path.join(__dirname, ".")));
 
-  app.use('/api', metricsRoute);
+  app.use("/api", metricsRoute);
 
-  app.get('*', (req, res) => {
-    console.log('Request received', path.join(__dirname, '../../dist/index.html'));
-    res.sendFile(path.join(__dirname, '../../dist/index.html'))
-  }
-  );
+  app.get("/favicon.svg", (req, res) => {
+    console.log("Favicon Request received " + req.url);
+    res.sendFile(path.join(__dirname, "../../dist/favicon.svg"));
+  });
+  app.get("/", (req, res) => {
+    console.log("Request received " + req.url);
+    res.sendFile(path.join(__dirname, "../../dist/index.html"));
+  });
 
   return app;
 };
